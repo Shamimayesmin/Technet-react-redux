513dee66dd744bc6e2bf95e8c228c42dde8674ed
Shamimayesmin <shamimayesmin144@gmail.com>
2023-08-28 18:27:18 +0600

    total count and checkout

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -3,6 +3,7 @@ import { Label } from '@/components/ui/label';
 import { Slider } from '@/components/ui/slider';
 import { Switch } from '@/components/ui/switch';
 import { useToast } from '@/components/ui/use-toast';
+import { useAppSelector } from '@/redux/hook';
 import { IProduct } from '@/types/globalTypes';
 import { useEffect, useState } from 'react';
 
@@ -17,12 +18,9 @@ export default function Products() {
 
   const { toast } = useToast();
 
-  //! Dummy Data
 
-  const status = true;
-  const priceRange = 100;
-
-  //! **
+  const {priceRange, status} = useAppSelector((state)=>state.product)
+ 
 
   const handleSlider = (value: number[]) => {
     console.log(value);
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,14 @@
 import {configureStore} from "@reduxjs/toolkit"
 import cartReducer from "./features/cart/cartSlice";
+import productReducer from "./features/products/productSlice";
 
 const store = configureStore({
     reducer : {
         cart: cartReducer,
+        product: productReducer
     },
 })
+
 
 export type RootState = ReturnType<typeof store.getState>
 
